fad3a08b2740d52774c1554f167c56845ce3d60f
Kamalesh <35285397+kamalesh777@users.noreply.github.com>
2023-03-10 18:05:48 +0530

    Update DropzoneUi.tsx

--- DropzoneUi.tsx
+++ DropzoneUi.tsx
@@ -5,38 +5,38 @@ import { Dropzone, FileItem } from "@dropzone-ui/react";
 function App() {
   const [files, setFiles] = useState([]);
   const [imageArr, setImageArr] = useState([])
+
   const updateFiles = (incommingFiles) => {
-    //do something with the files
-    setFiles(incommingFiles);
-    //even your own upload implementation
-  };
+    const filesArr = files.map((item) => item.file.name)
+    incommingFiles.map((item) => !filesArr.includes(item.file.name) &&
+      setFiles([...files, item])
+    );
+  }
+  
   const removeFile = (id) => {
     setFiles(files.filter((x) => x.id !== id));
   };
-  const getBase64 = (file, cb) => { //eslint-disable-line
-    const reader = new FileReader()
-    if (!!file) {
-      reader.readAsDataURL(file)
-      reader.onload = function () {
-        cb(reader.result)
-      }
-      reader.onerror = function (error) {
-        console.log('Error: ', error) //eslint-disable-line
-      }
-    }
-  }
+
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
+
   useEffect(() => {
     files.map((item) => {
-      return getBase64(item.file, (img) => {
-        const fileObj = {
-          name: item.file.name,
-          size: item.file.size,
-          type: item.file.type,
-          thumbnailUrl: img,
-        }
-        setImageArr((prevImages) => [...prevImages, fileObj])
-      })
-    })    
+        return getBase64(item.file, (img) => {
+          const fileObj = {
+            id: item.id,
+            name: item.file.name,
+            size: item.file.size,
+            type: item.file.type,
+            thumbnailUrl: img,
+          }
+          setImageArr([...imageArr, fileObj]) 
+        })
+    })
+     
   }, [files])
 
   console.log(imageArr)
@@ -48,7 +48,7 @@ function App() {
     >
       {files.length > 0 &&
         files.map((file) => (
-          <FileItem {...file} onDelete={removeFile} key={file.id} info />
+          <FileItem preview {...file} onDelete={removeFile} key={file.id} info />
         ))}
     </Dropzone>
   );
